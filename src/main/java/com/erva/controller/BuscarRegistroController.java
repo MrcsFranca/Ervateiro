ad4875ab2796747149ad3626266514355bb06643
luskaQ <lucasfpolidorio@gmail.com>
2025-10-23 18:03:46 -0300

    Comecando a implementar a busca de entregas

---
+++ src/main/java/com/erva/controller/BuscarRegistroController.java
@@ -0,0 +1,89 @@
+package com.erva.controller;
+
+
+import javafx.event.ActionEvent;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import com.erva.model.Entrega;
+import com.erva.DAO.EntregaDAO;
+import com.erva.DAO.EntregaDAOJDBC;
+
+public class BuscarRegistroController {
+
+    @FXML
+    private RadioButton CultivadaRadioBtn;
+
+    @FXML
+    private DatePicker FiltroDatePicker;
+
+    @FXML
+    private RadioButton MistaRadioBtn;
+
+    @FXML
+    private RadioButton NativaRadioBtn;
+
+    @FXML
+    private TextField PesquisarFornecedorTextField;
+
+    @FXML
+    private TextField PesquisarMotoristaTextField;
+
+    @FXML
+    private Button btnBuscar;
+
+    @FXML
+    private Button btnCadastros;
+
+    @FXML
+    private Button btnEntregas;
+
+    @FXML
+    void FiltrarCultivada(ActionEvent event) {
+
+    }
+
+    @FXML
+    void FiltrarData(ActionEvent event) {
+
+    }
+
+    @FXML
+    void FiltrarMista(ActionEvent event) {
+
+    }
+
+    @FXML
+    void FiltrarNativa(ActionEvent event) {
+
+    }
+
+    @FXML
+    void PesquisarFornecedor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void PesquisarMotorista(ActionEvent event) {
+
+    }
+
+    @FXML
+    void RealizarBusca(ActionEvent event) {
+
+    }
+
+    @FXML
+    void acessarCadastros(ActionEvent event) {
+
+    }
+
+    @FXML
+    void acessarEntregas(ActionEvent event) {
+
+    }
+
+}
